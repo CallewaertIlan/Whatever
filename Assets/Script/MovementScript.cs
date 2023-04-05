06f73faacff0fb8ff5e36d440dfabd953f97a34a
unknown <icallewaert@gamingcampus.local>
2023-04-05 12:18:58 +0200

    movement player test

--- Assets/Script/MovementScript.cs
+++ Assets/Script/MovementScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float movement = Input.GetAxis("Vertical") * speed;
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(0, 0, movement);
     }
 }
