5d6d7e77bd3ff0adbfeb130a2e876a1856ffd056
unknown <mozanne@gamingcampus.local>
2023-04-05 15:29:12 +0200

    add controler with meta quest 2

--- Assets/Script/MovementScript.cs
+++ Assets/Script/MovementScript.cs
@@ -1,25 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private float rotationSpeed;
+
+    private ActionsGameplay actions;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        actions = new ActionsGameplay();
+        actions.gameplay.drag.performed += Drag;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movement = Input.GetAxis("Vertical") * speed;
+        transform.Translate(actions.gameplay.move.ReadValue<Vector2>().x * Time.deltaTime * speed, 0, actions.gameplay.move.ReadValue<Vector2>().y * Time.deltaTime * speed);
+    }
 
-        movement *= Time.deltaTime;
+    private void Drag(InputAction.CallbackContext context)
+    {
+        Debug.Log("Drag");
+    }
 
-        transform.Translate(0, 0, movement);
+    void OnEnable()
+    {
+        actions.gameplay.Enable();
+    }
+    void OnDisable()
+    {
+        actions.gameplay.Disable();
     }
 }
